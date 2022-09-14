9763f944a590fcd8fcdc5192979391d0728a9d5d
wjc <641453202@qq.com>
2022-09-14 22:07:50 +0800

    9.14.3

--- lodash/j_c_wang-lodash.js
+++ lodash/j_c_wang-lodash.js
@@ -8,5 +8,66 @@ var j_c_wang = {
     return result
   }
 
+  compact: function (array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
+  }
 
+  concat: function (array, ...values) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      result.push(array[i])
+    }
+    for (var j = 0; j < values.length; j++) {
+      if (Array.isArray(values[j])) {
+        for (var k = 0; k < values[j].length; k++) {
+          result.push(values[j][k])
+        }
+      } else {
+        result.push(values[j])
+      }
+    }
+    return result
+  }
+
+  fill: function (array,value,start = 0,end = array.length) {
+    for (var i = start; i < end; i++) {
+      array[i] = value
+    }
+    return array
+  }
+
+  drop: function(array,n = 1) {
+    for (var i = 0; i < n; i++) {
+      array.shift()
+      if (array === []) {
+        break
+      }
+    }
+    return array
+  }
+
+  findIndex: function (array, predicate = _.identity,fromIndex = 0) {
+    for (var i = fromIndex; i < array.length; i++) {
+      if (predicate(array[i])) {
+        return i
+      } else {
+        for (var key in predicate) {
+          if (key in array[i]) {
+            return i + 1
+          }
+        }
+      }
+    }
+    return -1
+  }
+
+  findLastIndex: function() {
+
+  }
 }
